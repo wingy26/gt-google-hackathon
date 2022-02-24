b0c2a9d81baef1016926b2ee92e1785d384f9013
Wingy Ng <wingyng26@gmail.com>
2022-02-24 11:43:16 +0800

    Commit new strategy

--- src/main/kotlin/hello/KotlinApplication.kt
+++ src/main/kotlin/hello/KotlinApplication.kt
@@ -12,6 +12,59 @@ import reactor.core.publisher.Mono
 @SpringBootApplication
 class KotlinApplication {
 
+    private class Directions(
+        val direction: String,
+        val beingHitDirection: String,
+        val left: String,
+        val right: String,
+        val findingAlgorithm: (myState: PlayerState, distance: Int) -> (itState: PlayerState) -> Boolean
+    )
+
+    companion object {
+        private const val HIT = "H"
+        private const val BLOCK = "B"
+        private const val EMPTY = "E"
+
+        private const val NORTH = "N"
+        private const val EAST = "E"
+        private const val SOUTH = "S"
+        private const val WEST = "W"
+
+//        private val directions = listOf(
+//            NORTH to SOUTH,
+//            EAST to WEST,
+//            SOUTH to NORTH,
+//            WEST to EAST
+//        )
+
+        private val directions = mapOf(
+            NORTH to Directions(
+                NORTH,
+                SOUTH,
+                WEST,
+                EAST
+            ) { myState: PlayerState, distance: Int -> { it.x == myState.x && it.y == myState.y - distance } },
+            EAST to Directions(
+                EAST,
+                WEST,
+                NORTH,
+                SOUTH
+            ) { myState: PlayerState, distance: Int -> { it.y == myState.y && it.x == myState.x + distance } },
+            SOUTH to Directions(
+                SOUTH,
+                NORTH,
+                EAST,
+                WEST
+            ) { myState: PlayerState, distance: Int -> { it.x == myState.x && it.y == myState.y + distance } },
+            WEST to Directions(
+                WEST,
+                EAST,
+                SOUTH,
+                NORTH
+            ) { myState: PlayerState, distance: Int -> { it.y == myState.y && it.x == myState.x - distance } },
+        )
+    }
+
     @Bean
     fun routes() = router {
         GET {
@@ -20,10 +73,47 @@ class KotlinApplication {
 
         POST("/**", accept(APPLICATION_JSON)) { request ->
             request.bodyToMono(ArenaUpdate::class.java).flatMap { arenaUpdate ->
-                println(arenaUpdate)
-                ServerResponse.ok().body(Mono.just(listOf("F", "R", "L", "T").random()))
+                val myState = arenaUpdate.arena.state[arenaUpdate._links.self.href]!!
+                val currentHitState = findHitCountAtState(myState, arenaUpdate.arena)
+                val willBeHitCount = currentHitState.values.count { it == HIT }
+
+                val response = when (willBeHitCount) {
+                    0, 1 -> {
+                        if (currentHitState[myState.direction] == BLOCK) "T"
+                        else if (currentHitState[directions[myState.direction]!!.left] == BLOCK) "L"
+                        else if (currentHitState[directions[myState.direction]!!.right] == BLOCK) "R"
+                        else "F"
+                    }
+                    else -> "F"
+                }
+
+                ServerResponse.ok().body(Mono.just(response))
             }
         }
+    }
+
+    private fun findHitCountAtState(myState: PlayerState, arena: Arena): Map<String, String> {
+        val targetedPlayers = arena.state.values
+            .filter {
+                (it.x == myState.x && it.y in (myState.y - 3)..(myState.y + 3))
+                        || (it.y == myState.y && it.x in (myState.x - 3)..(myState.x + 3))
+            }
+
+        val currentHitState = mutableMapOf<String, String>()
+        (1..3).forEach { distance ->
+            directions.values.forEach { direction ->
+                val hit = targetedPlayers.find(direction.findingAlgorithm(myState, distance))?.direction
+
+                if (currentHitState[direction.direction] != HIT && currentHitState[direction.direction] != BLOCK) {
+                    when (hit) {
+                        direction.beingHitDirection -> currentHitState[direction.direction] = HIT
+                        null -> currentHitState[direction.direction] = EMPTY
+                        else -> currentHitState[direction.direction] = BLOCK
+                    }
+                }
+            }
+        }
+        return currentHitState
     }
 }
 
