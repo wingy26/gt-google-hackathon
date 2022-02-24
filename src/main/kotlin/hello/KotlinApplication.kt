344e8d9d0e14e087e76c473df97b7992ff3efe30
Wingy <wingyng26@gmail.com>
2022-02-24 02:48:04 +0000

    first commit

---
+++ src/main/kotlin/hello/KotlinApplication.kt
@@ -0,0 +1,38 @@
+package hello
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.http.MediaType.APPLICATION_JSON
+import org.springframework.web.reactive.function.server.ServerResponse
+import org.springframework.web.reactive.function.server.body
+import org.springframework.web.reactive.function.server.router
+import reactor.core.publisher.Mono
+
+@SpringBootApplication
+class KotlinApplication {
+
+    @Bean
+    fun routes() = router {
+        GET {
+            ServerResponse.ok().body(Mono.just("Let the battle begin!"))
+        }
+
+        POST("/**", accept(APPLICATION_JSON)) { request ->
+            request.bodyToMono(ArenaUpdate::class.java).flatMap { arenaUpdate ->
+                println(arenaUpdate)
+                ServerResponse.ok().body(Mono.just(listOf("F", "R", "L", "T").random()))
+            }
+        }
+    }
+}
+
+fun main(args: Array<String>) {
+    runApplication<KotlinApplication>(*args)
+}
+
+data class ArenaUpdate(val _links: Links, val arena: Arena)
+data class PlayerState(val x: Int, val y: Int, val direction: String, val score: Int, val wasHit: Boolean)
+data class Links(val self: Self)
+data class Self(val href: String)
+data class Arena(val dims: List<Int>, val state: Map<String, PlayerState>)
